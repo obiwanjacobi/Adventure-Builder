57c85d29fb696841e2875039e3ff7b3af4fe6e63
Marc Jacobi <Marc.Jacobi@macaw.nl>
2024-11-07 17:10:21 +0100

    ping

---
+++ Jacobi.AdventureBuilder.ApiService/PingEndpoint.cs
@@ -0,0 +1,19 @@
+﻿using FastEndpoints;
+
+namespace Jacobi.AdventureBuilder.ApiService;
+
+public sealed record TestResponse(string Status);
+
+public sealed class PingEndpoint : EndpointWithoutRequest<TestResponse>
+{
+    public override void Configure()
+    {
+        Get("/");
+        AllowAnonymous();
+    }
+
+    public override Task HandleAsync(CancellationToken ct)
+    {
+        return SendOkAsync(new TestResponse("OK"));
+    }
+}
