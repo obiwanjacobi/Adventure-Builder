993b91a23acfe4ba63ee9e8552ec070546d5fd77
Marc Jacobi <Marc.Jacobi@macaw.nl>
2024-12-28 17:59:29 +0100

    ui enhancements

--- Jacobi.AdventureBuilder.Web/Features/StoryLine.razor.cs
+++ Jacobi.AdventureBuilder.Web/Features/StoryLine.razor.cs
@@ -1,5 +1,6 @@
 ﻿using Jacobi.AdventureBuilder.GameContracts;
 using Microsoft.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
 
 namespace Jacobi.AdventureBuilder.Web.Features;
 
@@ -7,4 +8,17 @@ public partial class StoryLine : ComponentBase
 {
     [Parameter]
     public IReadOnlyList<PlayerLogLine> Items { get; set; } = [];
+
+    public Icon GetIcon(PlayerLogLineKind kind)
+    {
+        return kind switch
+        {
+            PlayerLogLineKind.Asset => new Icons.Regular.Size20.Notebook(),
+            PlayerLogLineKind.Command => new Icons.Regular.Size20.ArrowCurveUpRight(),
+            PlayerLogLineKind.NonPlayerCharacter => new Icons.Regular.Size20.Person(),
+            PlayerLogLineKind.Passage => new Icons.Regular.Size20.Location(),
+            PlayerLogLineKind.Player => new Icons.Regular.Size20.PersonHome(),
+            _ => new Icons.Regular.Size20.Box(),
+        };
+    }
 }
